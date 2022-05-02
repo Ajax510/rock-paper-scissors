5bb8fc153d654f9895c1f854dd662e6cc042d467
Alex I <alextheclaw@hotmail.com>
2022-05-02 16:17:30 -0700

    Switched from app.js to script.js

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// Game Begins
+
+// Computer choice, returns rock, paper, or scissors.
+const computerPlay = () => {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1) {
+    return "rock";
+  } else if (computerChoice === 2) {
+    return "paper";
+  } else if (computerChoice === 3) {
+    return "scissors";
+  }
+};
+
+// Plays one round of the game and returns a string
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return `Game has resulted in a TIE.`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+};
+
+const game = () => {
+  for (let i = 1; i <= 5; i++) {
+    console.log(`Round #${i}`);
+    let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    let computerSelection = computerPlay();
+    console.log(`Player: ${playerSelection}`);
+    console.log(`Computer: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`-------------------`);
+  }
+};
+
+console.log(game());
