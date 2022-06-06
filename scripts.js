bc350829fc33007c13f6f7623cfd78da2a0f750b
Alex I <alextheclaw@hotmail.com>
2022-06-06 14:41:20 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,82 +25,21 @@ const playRound = (playerSelection, computerSelection) => {
   }
 };
 
-// This is for the continer class and will be used later.
-const container = document.querySelector(".container");
-
-// The function for when you click ROCK.
-const rockPlay = () => {
-  let computerSelection = computerPlay();
-  let round = playRound("rock", computerSelection);
-  results(round);
-  game(round);
-};
-
-// Selecting the rockButton, then adding an eventListener
-// then directing you to the rockPlay function.
-const rockButton = document.querySelector("#rock");
-rockButton.addEventListener("click", rockPlay);
-
-// The function for when you click PAPER.
-const paperPlay = () => {
-  let computerSelection = computerPlay();
-  let round = playRound("paper", computerSelection);
-  results(round);
-  game(round);
-};
-
-// Selecting the paperButton, then adding an eventListener
-// then directing you to the paperPlay function.
-const paperButton = document.querySelector("#paper");
-paperButton.addEventListener("click", paperPlay);
-
-// The function for when you click SCISSORS.
-const scissorsPlay = () => {
-  let computerSelection = computerPlay();
-  let round = playRound("scissors", computerSelection);
-  results(round);
-  game(round);
-};
-
-// Selecting the scissorsButton, then adding an eventListener
-// directing you to the scissorsPlay function.
-const scissorsButton = document.querySelector("#scissors");
-scissorsButton.addEventListener("click", scissorsPlay);
-
-// This function will display the string passed to it from the specific eventListener
-const results = (resultsToDisplay) => {
-  const content = document.querySelector(".content");
-  content.innerText = `${resultsToDisplay}`;
-  container.appendChild(content);
-};
-
-// This function will play the game 5 rounds and declare a winner.
-const game = (playerSelection, computerSelection) => {
+const game = () => {
   let playerScore = 0;
   let computerScore = 0;
-  const displayPlayerScore = document.querySelector(".playerScore");
-  const displayComputerScore = document.querySelector(".computerScore");
-  const displayFinalScore = document.querySelector(".finalScore");
-
   for (let i = 1; i <= 5; i++) {
-    let roundResults = playRound(playerSelection, computerSelection);
-    if (roundResults.includes("win")) {
-      playerScore[i] += 1;
-      displayPlayerScore.innerText = `Player Score: ${playerScore}${[i]}`;
-      container.appendChild(displayPlayerScore);
-    } else if (roundResults.includes("lose")) {
-      computerScore[i] += 1;
-      displayComputerScore.innerText = `Computer Score: ${computerScore}${[i]}`;
-    } else if (roundResults.includes("TIE")) {
-      continue;
-    }
-  }
-  console.log("Final Score", computerScore, playerScore);
-  if (playerScore > computerScore) {
-    displayFinalScore.innerText = `You win! the Game!`;
-  } else if (playerScore < computerScore) {
-    displayFinalScore.innerText = `You lost the game.`;
-  } else if (playerScore === computerScore) {
-    displayFinalScore.innerText = `Game was a draw.`;
+    console.log(`Round #${i}`);
+    //let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    let computerSelection = computerPlay();
+
+    if ()
+
+    console.log(`Player: ${playerSelection}`);
+    console.log(`Computer: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`-------------------`);
   }
 };
+
+game();
